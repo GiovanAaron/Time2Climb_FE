307ce24f6f8d111e2dc34afaaa91ee65db3a9562
Nina <gadgetgeekette@hotmail.co.uk>
2024-07-09 11:41:25 +0100

    Updated map markers

--- components/screens/SessionListScreen.jsx
+++ components/screens/SessionListScreen.jsx
@@ -11,14 +11,14 @@ export default function SessionListScreen({ navigation }) {
   // TODO: temp data hard coded until BE is integrated (see MapScreen as an example)
   const tempMapData = {
     userLocations: [
-      { id: 1, latitude: 53.98423, longitude: -1.43245, numOfSessions: 1 },
-      { id: 2, latitude: 52.28423, longitude: -1.83154, numOfSessions: 2 },
-      { id: 3, latitude: 52.98423, longitude: -2.45304, numOfSessions: 3 },
-      { id: 4, latitude: 53.347581, longitude: -1.459631, numOfSessions: 5 }
+      { id: 1, latitude: 53.98423, longitude: -1.43245, numOfSessions: 1, wall_name: 'The Matrix' },
+      { id: 2, latitude: 52.28423, longitude: -1.83154, numOfSessions: 2, wall_name: 'Virgin Active' },
+      { id: 3, latitude: 52.98423, longitude: -2.45304, numOfSessions: 3, wall_name: 'White Hall Centre' },
+      { id: 4, latitude: 53.347581, longitude: -1.459631, numOfSessions: 3, wall_name: 'The Foundry Climbing Centre' }
     ],
     gymLocations: [
-      { id: 1, latitude: 53.28423, longitude: -1.953013 },
-      { id: 2, latitude: 53.148291, longitude: -1.202161 }
+        { id: 1, latitude: 53.28423, longitude: -1.953013, wall_name: 'The Climbing Works' },
+        { id: 2, latitude: 53.148291, longitude: -1.202161, wall_name: 'The Adventure Hub' }
     ],
     mapCentreLatitude: null, // TODO: set to user's location
     mapCentreLongitude: null, // TODO: set to user's location
